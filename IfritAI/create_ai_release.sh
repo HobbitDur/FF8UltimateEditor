d0ff0f62e7fc42a6e81d71289ac5effa0e6fd40d
HobbitDur <ludovic791@gmail.com>
2024-12-27 15:47:18 +0100

    Changing to submodule

--- IfritAI/create_ai_release.sh
+++
@@ -1,5 +0,0 @@
-./.venv/Scripts/pyinstaller.exe -n ifrit-ai -F --specpath release/build --distpath release --workpath release/build --paths venv/Lib/site-packages --onefile --noconsole --icon=../../Resources/icon.ico main.py;
-mkdir -p release/OriginalFiles/;
-cp -r Resources/ release/;
-cp -r OriginalFiles/ release/;
-rm -r release/build
